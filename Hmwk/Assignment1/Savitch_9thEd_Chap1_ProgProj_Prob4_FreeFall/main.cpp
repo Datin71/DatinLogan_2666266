a29f54117374e232f93cf4cc4fbb10fb886290e7
Datin71 <datin71@gmail.com>
2016-09-12 20:29:46 -0700

    Signed-off-by: Datin71 <datin71@gmail.com>

--- Hmwk/Assignment1/Savitch_9thEd_Chap1_ProgProj_Prob4_FreeFall/main.cpp
+++ Hmwk/Assignment1/Savitch_9thEd_Chap1_ProgProj_Prob4_FreeFall/main.cpp
@@ -1,47 +1,47 @@
 /* 
  * File:   main
  * Author: Logan Datin
- * Created on September 12, 2016, 10:27 AM
- * Purpose:  Calculate Distance dropped in freefall
+ * Created on September 10, 2016, 7:46 PM
+ * Purpose:  Determine the distance traveled when dropped
  */
 
 //System Libraries Here
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 //User Libraries Here
 
 //Global Constants Only, No Global Variables
 //Like PI, e, Gravity, or conversions
-const float GRAVITY=9.8; //Acceleration in meters per second squared
+const float GRAVITYm=9.8; //Acceleration in meters per second squared
+const float GRAVITYs=32;  //Acceleration in feet per second squared
 
 //Function Prototypes Here
 
 //Program Execution Begins Here
 int main(int argc, char** argv) {
     //Declare all Variables Here
-    float time;//time as an input in seconds
-    float disFell;  //Distance in free fall in meters
-    int places; //Places to round the answer
+    float time;    //time as an input in seconds
+    float dsFllm;  //Distance fallen in meters
+    float dsFlls;  //Distance  fallen in feet
+    
     
     
     //Input or initialize values Here
     cout<<"This problem determines distance dropped in freefall"<<endl;
     cout<<"Input the time in freefall"<<endl;
     cin>>time;
-    cout<<"How many decimal places to round the result"<<endl;
-    cin>>places;
+    
     //Process/Calculations Here
-    disFell=GRAVITY*time*time/2;
-    float shift=pow(10,places);
-    int round1=disFell*shift+0.5;
-    disFell=round1/shift;
+    dsFllm=GRAVITYm*time*time/2;
+    dsFlls=GRAVITYs*time*time/2;
     
     //Output Located Here
-    cout<<"Gravity = "<<GRAVITY<<" ft/sec^2"<<endl;
+    cout<<"Gravity Standard = "<<GRAVITYs<<" ft/sec^2"<<endl;
+    cout<<"Gravity Metric = "<<GRAVITYm<<" m/sec^2"<<endl;
     cout<<"Time fallen = "<<time<<" seconds"<<endl;
-    cout<<"Distance Traveled = "<<disFell<<" Meters"<<endl;
+    cout<<"Distance Traveled = "<<dsFlls<<" Feet"<<endl;
+    cout<<"Distance Traveled = "<<dsFllm<<" Meters"<<endl;
     
 
     //Exit
