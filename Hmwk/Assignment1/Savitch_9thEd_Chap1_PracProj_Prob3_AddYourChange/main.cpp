2509508e980d81297d0a565a36a9004c5cdffb4f
Datin71 <datin71@gmail.com>
2016-09-13 18:49:52 -0700

    Signed-off-by: Datin71 <datin71@gmail.com>

--- Hmwk/Assignment1/Savitch_9thEd_Chap1_PracProj_Prob3_AddYourChange/main.cpp
+++ Hmwk/Assignment1/Savitch_9thEd_Chap1_PracProj_Prob3_AddYourChange/main.cpp
@@ -18,21 +18,21 @@ using namespace std;  //Name-space used in the System Library
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declaration of Variables
-    int quarter;
-    int dime;
-    int nickel;
-    int ttlcent;
+    int quarter;    //how many quarters - user defined
+    int dime;       //how many dimes - user input
+    int nickel;     //how many nickels - user input
+    int ttlcent;    //total amount of cents
     //Input values
-    cout<<"How many quarters?"<<endl;
-    cin>>quarter;
-    cout<<"How many dimes?"<<endl;
-    cin>>dime;
-    cout<<"How many nickels?"<<endl;
-    cin>>nickel;
+    cout<<"How many quarters?"<<endl;   //prompt user for how many quarters
+    cin>>quarter;                       //enter the number of quarters
+    cout<<"How many dimes?"<<endl;      //prompt user for how many dimes
+    cin>>dime;                          //enter the number of dimes
+    cout<<"How many nickels?"<<endl;    //prompt user for how many nickels
+    cin>>nickel;                        //enter number of nickels
     //Process values -> Map inputs to Outputs
-    ttlcent = quarter*25 + dime*10 + nickel*5;
+    ttlcent = quarter*25 + dime*10 + nickel*5;  //calculate the total number of cents
     //Display Output
-    cout<<"You have "<<ttlcent<<" cents."<<endl;
+    cout<<"You have "<<ttlcent<<" cents."<<endl;//display total number amount of cents
     //Exit Program
     return 0;
 }
