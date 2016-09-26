5e5f81ff15e5da7ff81324a77a604c75b391bada
rcc <rcc>
2016-09-26 10:23:11 -0700

    Signed-off-by: rcc <rcc>

--- Lab/GradeIfElse/main.cpp
+++ Lab/GradeIfElse/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Logan Datin
  * Created on September 26, 2016, 9:58 AM
- * Purpose:  Input a score, output a grade using if
+ * Purpose:  input a score, output a grade using if-else
  */
 
 //System Libraries Here
@@ -21,19 +21,21 @@ int main(int argc, char** argv) {
     //Declare all Variables Here
     char grade;
     unsigned short score;
+    
     //Input or initialize values Here
     cout<<"Input the score, receive your grade as output"<<endl;
     cin>>score;
+    
     //Process/Calculations Here
     if(score>=90)grade='A';
-    if(score<90&&score>=80)grade='B';
-    if(score<80&&score>=70)grade='C';
-    if(score<70&&score>=60)grade='D';
-    if(score<60)grade='F';
+    else if(score>=80)grade='B';
+    else if(score>=70)grade='C';
+    else if(score>=60)grade='D';
+    else grade='F';
+    
     //Output Located Here
     cout<<"Your Grade = "<<grade<<" with a score = "<<score<<endl;
 
     //Exit
     return 0;
 }
-
