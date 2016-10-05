ebd9d115e9c0e2438a16c14a782a006e961d4931
datin71 <datin71@gmail.com>
2016-10-05 15:18:05 -0700

    Signed-off-by: datin71 <datin71@gmail.com>

---
+++ Hmwk/Assignment3/Gaddis_8thEd_Chap4_Prob2_RomanNumeral/main.cpp
@@ -0,0 +1,30 @@
+/* 
+   File:   main
+   Author: Logan Datin
+   Created on September 13, 2016, 6:27 PM
+   Purpose:  
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    
+    //Input values
+    
+    //Process values -> Map inputs to Outputs
+    
+    //Display Output
+
+    //Exit Program
+    return 0;
+}
