8b06d4379acce96541d61aa6ac6ef3b9aa030d0c
rcc <rcc>
2016-10-17 09:39:03 -0700

    Signed-off-by: rcc <rcc>

--- Hmwk/Assignment4/Gaddis_8thEd_Chap5_Prob5_MembershipFees/nbproject/Makefile-impl.mk
+++ Hmwk/Assignment4/Gaddis_8thEd_Chap5_Prob5_MembershipFees/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=CPPTemplate
+PROJECTNAME=Gaddis_8thEd_Chap5_Prob5_MembershipFees
 
 # Active Configuration
 DEFAULTCONF=Debug
