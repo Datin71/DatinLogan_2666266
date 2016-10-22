e1422cf273846a4cdfd0388aa99a85315a53c461
datin71 <datin71@gmail.com>
2016-10-21 22:17:59 -0700

    Signed-off-by: datin71 <datin71@gmail.com>

--- Proj/midterm/Midterm_Prob6_Sequence/main.cpp
+++ Proj/midterm/Midterm_Prob6_Sequence/main.cpp
@@ -19,53 +19,43 @@ int factorial(int x);
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declaration of Variables
-    int x;      //input number for equation
-    int n;      //the number of terms
-    int sum1=0;    //first result of the sequence
-    int y=0;      //alternate sign working variable
-    int z=0;     //Exponent number on sequence
+    int x;          //input number for equation
+    int n;          //the number of terms
+    float sum1=0;   //first result of the sequence
+    float sum;      //final result
+    int y=0;        //alternate sign working variable
+    int z=0;        //Exponent number on sequence and factorial
     
     //Prompt for inputs
     cout<<"This program will output the sum of 1 - (x^2)/2! + (x^4)/4! -(x^6)/6! + .........."<<endl;
     cout<<"where x is the number you input and n is the number of terms"<<endl;
-    cout<<"input a number"<<endl;
+    cout<<"input a number(x)"<<endl;
     cin>>x;
     cout<<"Input the number of terms"<<endl;
     cin>>n;            
     
-    //Process 
-    for(int i=0;i<n;i++){
-        cout<<"before"<<endl;
-        cout<<"x: "<<x<<endl;
-        cout<<"y: "<<y<<endl;
-        cout<<"z: "<<z<<endl;
-        cout<<"sum: "<<sum1<<endl;
+    //Process sequence
+    for(int i=0;i<n;i++){                           //repeat n number of times
+        z+=2;                                       //add 2 to z
+        y=(i%2==0)?-1:1;                            //Make sign alternate by changing y from +1 to -1
+        sum1=y*((pow(x,(z)))/factorial(z))+sum1;    //sum is equal to previous sum + the new sequence
         
-        y=(i%2==0)?-1:1; //Make sign alternate by changing y from +1 to -1
-        sum1=y*(pow(x,(z+2)))+sum1; //sum is equal to previous sum + the new sequence
-        z+=2;
-        
-        
-        cout<<"after"<<endl;
-        cout<<"x: "<<x<<endl;
-        cout<<"y: "<<y<<endl;
-        cout<<"z: "<<z<<endl;
-        cout<<"sum: "<<sum1<<endl;
     }
+    sum=sum1+1; //add one to sum1
     
-    //cout<<sum<<endl;
-    //cout<<x<<endl;
-    //cout<<y<<endl;
-    //for(int i=0;i<n;i++){  }
-        
-    
+    //Display output
+    cout<<"The total sum of the sequence is"<<sum<<endl;
 
     //Exit Program
     return 0;
 }
-
-
-
-int factorial(int x){
-    
+//000000011111111112222222222333333333344444444445555555555666666666677777777778
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+//*************************   factorial funtion*********************************
+//Purpose:  take a number and return factorial
+//Inputs:   integer from program
+//Output:   factorial of integer that was input
+//******************************************************************************
+int factorial(int f){
+  return (f==1||f==0)?1:factorial(f-1)*f;
 }
