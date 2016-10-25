920e25e76c8bfaac3ceafdfe09fdb3a47ab376c9
datin71 <datin71@gmail.com>
2016-10-24 21:12:59 -0700

    Signed-off-by: datin71 <datin71@gmail.com>

--- Class/CPP_TEMPLATE/main.cpp
+++ Class/CPP_TEMPLATE/main.cpp
@@ -1,31 +1,73 @@
-/* 
-   File:   main
-   Author: Logan Datin
-   Created on October 05, 2016, 4:37 PM
-   Purpose:  
- */
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <iomanip>
+#include <vector>
 
-//System Libraries
-#include <iostream>   //Input/Output objects
-using namespace std;  //Name-space used in the System Library
+const int empty    = 0;  // contains water
+const int occupied = 1;  // contains a ship
+const int missed   = 2;  // shot into ocean
+const int hit      = 3;  // shot and hit 
 
-//User Libraries
+using namespace std;
 
-//Global Constants
+int board[ 10 ][ 10 ]; // Two-dimensional array for gameboard.
 
-//Function prototypes
+void initialize_board( int array1[ 10 ][ 10 ] ) // Function to initialize the gameboard.
+{
+  // create a blank board   
+  for (int x=0; x<10; x++) {
+     for (int y=0; y<10; y++) {
+          array1[x][y] = occupied;
+           }     
+     }     
+}
 
-//Execution Begins Here!
-int main(int argc, char** argv) {
-    //Declaration of Variables
-    
-    
-    //Process values -> Map inputs to Outputs
-    
-    
-    //Display Output
-    
+void print_board(int array2[10][10]) {
+    for(char a = 'A'; a <= 'J'; a++) { //letter coordinates
+		cout << setw(5) << a;
+    }
+    cout << endl;
+ 
+    for(int i = 1; i <= 10; i++) { //number coordinates
+        if(i == 10)
+		cout << i;
+	else
+		cout << " " << i ;
+ 
+        for(int j = 0; j < 10 ; j++) {
+ 
+	    if(array2[i][j] == occupied || array2[i][j] == empty){
+				cout << setw(5) << " |" ;
+			}
+            else if(array2[i][j] == missed ) {
+                cout << setw(5) << "O|";
+			}
+	    else if(array2[i][j] == hit ) {
+		cout << setw(5) << "X|";
+	    }
+	}
+        cout << "\n";
+    }
+}
 
-    //Exit Program
-    return 0;
+class cShip //Unused so far
+{
+	int x1, y1, x2, y2; // The position of the front and back of the ship
+	int size;
+	int damage[]; // for storing the damage. 
+	public:
+	cShip(int x1, int y1, int x2, int y2, int size); // constructor
+	~cShip(); // destructor for destroying the damage array
+	bool isDestroyed(); // for polling the destroyed
+	bool is3(int x, int y); // for polling the 3		
+};
+
+int main()
+{
+	initialize_board(board);
+	determine_player_choice();
+	print_board(board);
+	
+	return 0;
 }
---
+++ Proj/Project1_Battleships/main.cpp
@@ -0,0 +1,34 @@
+/* 
+   File:   main
+   Author: Logan Datin
+   Created on October 24, 2016, 7:31 PM
+   Purpose:  Sink the battleships before time runs out
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    int time=100
+    
+    //Declare Scenario to User
+    cout<<"We have enemy battleships ahead!"<<endl;
+    cout<<"It is believed that they are calling for backup, eliminate them before"<<endl;
+    cout<<" they are able to call for reinforcements"<<endl;
+    cout<<"To mark a position to fire on, type row I# then column J#"<<endl;
+        
+    //Display Output
+    cout<<"I"
+
+    //Exit Program
+    return 0;
+}
