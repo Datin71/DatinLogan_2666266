785b3eae95f6366bc0e793959f6d953c5726e510
datin71 <datin71@gmail.com>
2016-11-06 14:14:42 -0800

    Signed-off-by: datin71 <datin71@gmail.com>

---
+++ Hmwk/Assignment5/Gaddis_8thEd_Chap6_Prob3_WinningDivision/main.cpp
@@ -0,0 +1,30 @@
+/* 
+   File:   main
+   Author: Logan Datin
+   Created on November 6, 2016, 2:14 PM
+   Purpose:  Determine the highest sales for a quarter between 4 divisions
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    
+    //Process
+    
+    //Output
+    
+    //Exit Program
+    return 0;
+}
+
