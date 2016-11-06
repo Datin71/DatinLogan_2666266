725bf766045cb7535e2e1abbd75f5687b6fdac79
datin71 <datin71@gmail.com>
2016-11-05 17:05:29 -0700

    Signed-off-by: datin71 <datin71@gmail.com>

---
+++ Lab/CPP_template/CPP_Template/main.cpp
@@ -0,0 +1,32 @@
+/* 
+   File:   main
+   Author: Logan Datin
+   Created on November 5, 2016, 5:21 PM
+   Purpose:  
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+ 
+    //Input values
+    
+       
+    //Process values -> Map inputs to Outputs
+    
+    
+    //Display Output
+   
+    //Exit Program
+    return 0;
+}
