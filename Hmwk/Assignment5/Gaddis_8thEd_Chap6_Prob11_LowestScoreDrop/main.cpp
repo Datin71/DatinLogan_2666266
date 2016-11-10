c69bbe1fd874a75fc18e8ac0bf8e0660e6aea5a1
datin71 <datin71@gmail.com>
2016-11-09 22:43:10 -0800

    Signed-off-by: datin71 <datin71@gmail.com>

---
+++ Hmwk/Assignment5/Gaddis_8thEd_Chap6_Prob11_LowestScoreDrop/main.cpp
@@ -0,0 +1,36 @@
+/* 
+   File:   main
+   Author: Logan Datin
+   Created on November 6, 2016, 6:19 PM
+   Purpose:  Lowest Score Drop
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+void score(float);    //Gets the score
+void ave(float);      //Calculates and displays the average of the highest 4 scores
+int low(float,float,float,float,float); //Finds the lowest score
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    float tScore;   //Test Score Number
+    
+    //Process to get results
+        
+    //Output
+    
+    //Exit Program
+    return 0;
+}
+
+void score(float){
+    //
+}
