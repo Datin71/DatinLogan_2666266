f97d6f93c6a7cc246a2a4fe48151620f33e34480
rcc <rcc>
2016-11-16 10:55:34 -0800

    Signed-off-by: rcc <rcc>

--- Hmwk/Assignment6/Gaddis_8thEd_Chap7_Prob3_ChipsAndSalsa/main.cpp
+++ Hmwk/Assignment6/Gaddis_8thEd_Chap7_Prob3_ChipsAndSalsa/main.cpp
@@ -1,12 +1,13 @@
 /* 
  * File:   main.cpp
- * Author: Dr. Mark E. Lehr
- * Created on July 19, 2016, 9:07 AM
- * Purpose:  Hello World Template
+ * Author: Logan Datin
+ * Created on November 16, 2016, 10:35 AM
+ * Purpose:  Different levels of salsa info
  */
 
 //System Libraries Here
 #include <iostream>
+#include <fstream>  //read in data from file
 using namespace std;
 
 //User Libraries Here
@@ -19,13 +20,14 @@ using namespace std;
 //Program Execution Begins Here
 int main(int argc, char** argv) {
     //Declare all Variables Here
-    
+    string salsa[5];    //Types of salsa
+    int sales[5];       //Sales for each type of salsa
     //Input or initialize values Here
-    
+   
     //Process/Calculations Here
     
     //Output Located Here
-    cout<<"Hello World"<<endl;
+    
 
     //Exit
     return 0;
